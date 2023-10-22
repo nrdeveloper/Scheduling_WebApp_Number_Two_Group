faef4e3859f29bc43c3ef45589744eea732ae711
Nahuel Robledo <nahuel.erobledo@gmail.com>
2023-10-21 23:58:44 -0700

    Chore: commiting project structure

---
+++ src/main/java/com/csis3275/SchedulingWebAppNumberTwoGroupApplication.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingWebAppNumberTwoGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingWebAppNumberTwoGroupApplication.class, args);
+	}
+
+}
