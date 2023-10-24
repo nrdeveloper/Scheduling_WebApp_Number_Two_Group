673a04f5813200249fe13572d838e55a77ada9f0
Vlad Kargaltsev <64346957+VladSpadey@users.noreply.github.com>
2023-10-23 18:11:29 -0700

    Feat: created LoginPage and it's controller

---
+++ src/main/java/com/csis3275/controllers/LogInPage_controller.java
@@ -0,0 +1,13 @@
+package com.csis3275.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogInPage_controller {
+	@GetMapping("/login")
+	public String loginPage() {
+		return ("login");
+	}
+	
+}
