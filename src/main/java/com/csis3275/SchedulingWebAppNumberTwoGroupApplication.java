1beb072afa507ec55b7a594669550674a9cd40fb
Vlad Kargaltsev <64346957+VladSpadey@users.noreply.github.com>
2023-10-23 17:55:38 -0700

    feat: Added initial setup for UserModel

--- src/main/java/com/csis3275/SchedulingWebAppNumberTwoGroupApplication.java
+++ src/main/java/com/csis3275/SchedulingWebAppNumberTwoGroupApplication.java
@@ -1,13 +1,34 @@
 package com.csis3275;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.csis3275.models.UserModel;
+import com.csis3275.repositories.I_UserRepository;
+
 @SpringBootApplication
-public class SchedulingWebAppNumberTwoGroupApplication {
+public class SchedulingWebAppNumberTwoGroupApplication implements CommandLineRunner{
+	private I_UserRepository userRepository;
+	
+	public SchedulingWebAppNumberTwoGroupApplication(I_UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulingWebAppNumberTwoGroupApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		if(userRepository.findAll().isEmpty()) {
+			userRepository.save(new UserModel("Sam Hill", "samhill@gmail.com", "ioqwhejr"));
+		}
+		
+		for (UserModel user : userRepository.findAll()) {
+			System.out.println(user);
+		}
+		
 	}
 
 }
