c34c5a05e22d6db8709108dc44819a6243e23bd3
Nahuel Robledo <nahuel.erobledo@gmail.com>
2023-10-29 00:12:15 -0700

    Revert "Merge pull request #14 from nrdeveloper/LogIn_FEATURE"
    This reverts commit 648f3e1438be051d2bb2566781ad9dc2dc0559f6, reversing
    changes made to 8eaaa20de42fa206c273cf788116783cb90af59e.

--- src/main/java/com/csis3275/SchedulingWebAppNumberTwoGroupApplication.java
+++ src/main/java/com/csis3275/SchedulingWebAppNumberTwoGroupApplication.java
@@ -1,29 +1,13 @@
 package com.csis3275;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.csis3275.models.UserModel;
-import com.csis3275.repositories.I_UserRepository;
-
 @SpringBootApplication
-public class SchedulingWebAppNumberTwoGroupApplication implements CommandLineRunner{
-	
-	@Autowired
-	I_UserRepository userRepository;
+public class SchedulingWebAppNumberTwoGroupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulingWebAppNumberTwoGroupApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		for (UserModel user : userRepository.findAll()) {
-			System.out.println(user);
-		}
-		
 	}
 
 }
