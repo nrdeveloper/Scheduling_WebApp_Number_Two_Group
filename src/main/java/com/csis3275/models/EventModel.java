7059f6a0bcc1b2bf839e3aa12fd9229466ab5418
Nahuel Robledo <nahuel.erobledo@gmail.com>
2023-11-12 00:29:56 -0800

    Refactor: Event class to include it within user

--- src/main/java/com/csis3275/models/EventModel.java
+++ src/main/java/com/csis3275/models/EventModel.java
@@ -20,11 +20,8 @@ public class EventModel {
 	@Field
 	private LocalDateTime finish;
 	
-	public EventModel(String id, String title, String description, LocalDateTime start, LocalDateTime finish) {
-		super();
-		this.id = id;
+	public EventModel(String title,LocalDateTime start, LocalDateTime finish) {
 		this.title = title;
-		this.description = description;
 		this.start = start;
 		this.finish = finish;
 	}
@@ -33,28 +30,12 @@ public class EventModel {
 		super();
 	}
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
 	public String getTitle() {
 		return title;
 	}
 
 	public void setTitle(String title) {
 		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
 	}
 
 	public LocalDateTime getStart() {
@@ -75,7 +56,7 @@ public class EventModel {
 
 	@Override
 	public String toString() {
-		return "Event [id=" + id + ", title=" + title + ", description=" + description + ", start=" + start
+		return "Event [title=" + title + ", start=" + start
 				+ ", finish=" + finish + "]";
 	} 	
 }
