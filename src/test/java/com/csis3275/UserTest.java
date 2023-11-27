ce086071d6428e22876209485cd938c00920ca41
Vlad Kargaltsev <64346957+VladSpadey@users.noreply.github.com>
2023-11-27 13:12:49 -0800

    feat: added User JUnit test case

---
+++ src/test/java/com/csis3275/UserTest.java
@@ -0,0 +1,81 @@
+package com.csis3275;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.csis3275.models.UserModel;
+import com.csis3275.services.PasswordUtils;
+import com.csis3275.services.UserImpl;
+
+@SpringBootTest
+class UserTest {
+
+    @Autowired
+    private UserImpl userService;
+
+    @Test
+    void testSuccessfulSignIn() {
+        String email = "test@example.com";
+        String password = "password";
+        UserModel user = new UserModel("Test User", email, password);
+
+        userService.addUser(user);
+
+        UserModel fetchedUser = userService.getUserByEmail(email);
+
+        assertEquals("Test User", fetchedUser.getName());
+        assertEquals(email, fetchedUser.getEmail());
+        assertTrue(PasswordUtils.verifyPassword(password, fetchedUser.getPassword()));
+        assertTrue(userService.authorizeUser(email, password));
+        
+        userService.deleteUser(email);
+    }
+    
+    @Test
+    void testUnsuccessfulSignIn() {
+        String email = "test@example.com";
+        String password = "password";
+        String incorrectPassowrd = "qweqweqwe";
+        UserModel user = new UserModel("Test User", email, password);
+
+        userService.addUser(user);
+
+        UserModel fetchedUser = userService.getUserByEmail(email);
+
+        assertEquals("Test User", fetchedUser.getName());
+        assertEquals(email, fetchedUser.getEmail());
+        assertFalse(PasswordUtils.verifyPassword(incorrectPassowrd, fetchedUser.getPassword()));
+        assertFalse(userService.authorizeUser(email, incorrectPassowrd));
+        
+        userService.deleteUser(email);
+    }
+    
+    @Test
+    void testCreatingAUserWithTheSameEmail() {
+    	// Can't have 2 users with the same email
+    	
+        String email = "test2@example.com";
+        UserModel user = new UserModel("Test User", email, "password");
+        UserModel user2 = new UserModel("Test User", email, "password");
+        
+        userService.addUser(user);
+        userService.addUser(user2);
+        
+        UserModel fetchedUser = userService.getUserByEmail(email);
+    }
+    
+    @Test
+    void testDeletingBeforeCreatingNewUser() {
+        String email = "test3@example.com";
+        UserModel user = new UserModel("Test User", email, "password");
+        userService.addUser(user);
+        userService.deleteUser(email);
+        UserModel user2 = new UserModel("Test User", email, "password");
+        userService.addUser(user2);
+        userService.deleteUser(email);
+    }
+   
+}
