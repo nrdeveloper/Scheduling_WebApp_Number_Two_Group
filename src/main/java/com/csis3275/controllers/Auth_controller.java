bd476852d680f6f73b3986ab1da72f9c3420ca36
Vlad Kargaltsev <64346957+VladSpadey@users.noreply.github.com>
2023-11-30 00:05:57 -0800

    chore: failed attempt to add toastify, deleted most of the code, committing this because can't switch to another branch otherwise

--- src/main/java/com/csis3275/controllers/Auth_controller.java
+++ src/main/java/com/csis3275/controllers/Auth_controller.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.csis3275.models.EventModel;
 import com.csis3275.models.UserModel;
@@ -160,8 +161,8 @@ public class Auth_controller {
 		user.setCity(city);
 		user.setLatitude(latitude);
 		user.setLongitude(longitude);
-		System.out.println("City was updated to " + user.getCity());
 		
+		System.out.println("City was updated to " + user.getCity());
 		return ("redirect:/");
 	}
 }
