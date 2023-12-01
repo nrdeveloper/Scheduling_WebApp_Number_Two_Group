bcb843e3acd02615f785646bf8f1d549b45d1065
Vlad Kargaltsev <64346957+VladSpadey@users.noreply.github.com>
2023-11-30 16:46:36 -0800

    feat: added weather API

---
+++ src/main/java/com/csis3275/services/WeatherAPI.java
@@ -0,0 +1,26 @@
+package com.csis3275.services;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class WeatherAPI {
+	public JsonNode fetchWeather(int latitude, int longitude) throws IOException {
+            String apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude +"&longitude=" +  longitude +"&current=precipitation,rain,showers&hourly=temperature_2m,rain,snowfall&timezone=auto&forecast_days=16";
+            URL url = new URL(apiUrl);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+    		InputStream responseStream = connection.getInputStream();
+    		ObjectMapper mapper = new ObjectMapper();
+    		JsonNode root = mapper.readTree(responseStream);
+    		connection.disconnect();
+    		return root;
+        }
+}
+
